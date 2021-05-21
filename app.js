7410ae84a6d8104da5c4d156ff5e7ec07103a9ae
ParkJuHyeon <terajoohyun@ajou.ac.kr>
2021-05-21 14:16:20 +0900

    feat: add body-parser to server

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+const bodyParser = require("body-parser");
 var logger = require('morgan');
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const compression = require("compression");
 const cors = require("cors");
 var app = express();
 
@@ -21,6 +21,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 app.use('/api', indexRouter);
 app.use('/auth', usersRouter);
 
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 var db = require("../lib/db");
+var getLevel = require("../lib/level");
 // db 설정해놨던거 가져오기
 
 /*************************       댓글 관련       ************************ */
@@ -9,12 +10,12 @@ router.get("/get_comment/:id", (req, res) => {
     var id = req.params.boardid;
     if (id === undefined) res.json({ success: false });
     else {
-      // db 의 product 에서 category, name, left 를 가져오기 위한 코드
+      // db 의 comment 에서 id, description, userid, createdate 를 가져오기 위한 코드
       db.query(
-        `select id, product_category, product_name, product_left from product where product_visible='true' and userid='${id}';`,
+        `select * from product where boardid='${id}';`,
         (err, results, field) => {
           if (err || results === undefined || results.length === 0) {
-            // 중간에 에러가 있거나, 상품이 없으면! {success: false} 반환
+            // 중간에 에러가 있거나, 댓글이 없으면! {success: false} 반환
             res.json({ success: false });
           } else {
             // 상품을 정상적으로 찾았으면 {list: results} 형태로 반환
@@ -33,20 +34,23 @@ router.post("/put_comment", (req, res) => {
     var boardid = req.body.boardid;
     var description = req.body.description;
     var userid = req.body.userid;
-    console.log("get barcode");
-    db.query(`select * from barcode`, (err, results, field) => {
-      if (err) res.json({ success: false });
-      else res.json({ success: true, list: results });
-    });
+    console.log("get comment");
+    db.query(
+      `insert into comment(boardid, description, userid) values(${boardid}, '${description}', '${userid}')`,
+      (err, results, field) => {
+        if (err) res.json({ success: false });
+        else res.json({ success: true });
+      }
+    );
   } catch (err) {
     res.json({ success: false });
   }
 });
-router.post("/delete_comment", (req, res) => {
+router.delete("/delete_comment/:id", (req, res) => {
   try {
-    var id = req.body.commentid;
+    var id = req.params.id;
     db.query(
-      `delete from barcode where barcode_value = '${barcode}' and userid='${id}'`,
+      `delete from comment where id = '${id}'`,
       (err, results, filed) => {
         if (err) res.json({ success: false });
         else res.json({ success: true });
@@ -57,37 +61,30 @@ router.post("/delete_comment", (req, res) => {
   }
 });
 
-
 /********************************      센터 정보 관리    ******************************** */
 router.get("/get_center", (req, res) => {
   try {
-    db.query(
-      `select B.barcode_value, B.product_id from (select id from product where userid='${id}') A join barcode B on A.id = B.product_id`,
-      (err, results, field) => {
-        if (err) {
-          console.log(err);
-          res.json({ success: false });
-        } else res.json({ success: true, list: results });
-      }
-    );
+    db.query(`select * from center`, (err, results, field) => {
+      if (err) {
+        console.log(err);
+        res.json({ success: false });
+      } else res.json({ success: true, list: results });
+    });
   } catch (err) {
     console.log(err);
     res.json({ success: false });
   }
 });
 
-
-
 /*****************************       게시판 정보 관리     ***************************** */
 
 router.post("/create_board", (req, res) => {
   try {
     var description = req.body.description;
-    var pid = req.body.boardid;
-    var count = req.body.userid;
+    var userid = req.body.userid;
     // var userid = req.body.userid;
     db.query(
-      `insert into barcode(barcode_value, product_id, count, userid) values('${barcode}', '${pid}','${count}','${userid}')`,
+      `insert into board(userid, description) values('${userid}', '${description}')`,
       (err, results, field) => {
         if (err) {
           console.log(err);
@@ -107,14 +104,14 @@ router.get("/view_board/:id", (req, res) => {
     var boardid = req.params.id;
     // db 의 product 테이블에 상품을 등록하기 위한 코드
     db.query(
-      `insert into product(product_category, product_name, product_left, product_visible, userid) values('${category}', '${name}', '${left}', '${visible}', '${userid}')`,
+      `update board set view = view + 1 where id = ${boardid}`,
       (err, results, field) => {
         if (err) {
           res.json({ success: false });
-          // 등록하는 과정에 에러가 있으면 {success: false} 반환
+          // 증가하는 과정에 에러가 있으면 {success: false} 반환
         } else {
           res.json({ success: true });
-          // 성공적으로 등록하였으면 {success : true} 반환
+          // 성공적으로 증가하였으면 {success : true} 반환
         }
       }
     );
@@ -129,14 +126,14 @@ router.delete("/view_board/:id", (req, res) => {
     var boardid = req.params.id;
     // db 의 product 테이블에 상품을 등록하기 위한 코드
     db.query(
-      `insert into product(product_category, product_name, product_left, product_visible, userid) values('${category}', '${name}', '${left}', '${visible}', '${userid}')`,
+      `update board set view = view - 1 where id = ${boardid}`,
       (err, results, field) => {
         if (err) {
           res.json({ success: false });
-          // 등록하는 과정에 에러가 있으면 {success: false} 반환
+          // 감소하는 과정에 에러가 있으면 {success: false} 반환
         } else {
           res.json({ success: true });
-          // 성공적으로 등록하였으면 {success : true} 반환
+          // 성공적으로 감소하였으면 {success : true} 반환
         }
       }
     );
@@ -152,17 +149,17 @@ router.get("/recommend_board/:id", (req, res) => {
 
     // db 의 product 테이블에 상품을 삭제하기 위한 코드
     db.query(
-      `delete from product where id='${p_id}' and userid='${userid}'`,
+      `update board set recommend = recommend + 1 where id = ${boardid}`,
       (err, results, field) => {
         if (err) {
           console.log(err);
           res.json({ success: false });
-          // 등록하는 과정에 에러가 있으면 {success: false} 반환
+          // 추천 증가하는 과정에 에러가 있으면 {success: false} 반환
         } else {
           res.json({
             success: true,
           });
-          // 성공적으로 삭제하였으면 {success : true} 반환
+          // 성공적으로 증가하였으면 {success : true} 반환
         }
       }
     );
@@ -179,7 +176,7 @@ router.delete("/recommend_board/:id", (req, res) => {
 
     // db 의 product 테이블에 상품을 삭제하기 위한 코드
     db.query(
-      `delete from product where id='${p_id}' and userid='${userid}'`,
+      `update board set recommend = recommend - 1 where id = ${boardid}`,
       (err, results, field) => {
         if (err) {
           console.log(err);
@@ -205,20 +202,29 @@ router.post("/levelup", (req, res) => {
     console.log("api create_product");
     var userid = req.body.id;
     var experience = req.body.experience;
-    
+
     // db 의 product 테이블에 상품을 편집하기 위한 코드
     db.query(
-      `update product set product_name='${name}', product_category='${categoryu}', product_left='${left}', product_visible='${visible}', userid='${userid}')`,
+      `update board set experience = experience + ${experience} where id = ${userid}`,
       (err, results, field) => {
         if (err) {
           res.json({ success: false });
           // 등록하는 과정에 에러가 있으면 {success: false} 반환
         } else {
           db.query(
-            `update barcode set barcode_value='${barcode}' where product_id='${name}'`,
+            `select experience from board where id = ${userid}`,
             (err, results, field) => {
-              if (err) res.json({ success: false });
-              else res.json({ success: true });
+              if (err)
+                res.json({
+                  success: false,
+                });
+              else
+                res.json({
+                  success: true,
+                  data: {
+                    tier: getLevel(results[0].experience),
+                  },
+                });
             }
           );
           // 성공적으로 편집하였으면 {success : true} 반환
