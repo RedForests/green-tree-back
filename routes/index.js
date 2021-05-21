f716849793eceb102249145c19979d73f7e71530
ParkJuHyeon <terajoohyun@ajou.ac.kr>
2021-05-21 23:39:30 +0900

    feat: make rank api

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var db = require("../lib/db");
 const jwt = require("../lib/jwt");
 const authUtil = require('../lib/auth').checkToken;
 var getLevel = require("../lib/level");
+const todoList = require('../lib/todolist');
 // db 설정해놨던거 가져오기
 const TOKEN_EXPIRED = -3;
 const TOKEN_INVALID = -2;
@@ -11,7 +12,7 @@ const TOKEN_INVALID = -2;
 
 
 /*************************       댓글 관련       ************************ */
-router.get("/get_comment/:id", (req, res) => {
+router.get("/get_comment/:id",authUtil, (req, res) => {
   try {
     var id = req.params.id;
     console.log(id);
@@ -37,7 +38,7 @@ router.get("/get_comment/:id", (req, res) => {
     res.json({ success: false });
   }
 });
-router.post("/put_comment", (req, res) => {
+router.post("/put_comment",authUtil, (req, res) => {
   try {
     var boardid = req.body.boardid;
     var description = req.body.description;
@@ -54,7 +55,7 @@ router.post("/put_comment", (req, res) => {
     res.json({ success: false });
   }
 });
-router.delete("/delete_comment/:id", (req, res) => {
+router.delete("/delete_comment/:id",authUtil, (req, res) => {
   
   try {
     var id = req.params.id;
@@ -88,7 +89,7 @@ router.get("/get_center", authUtil, async (req, res, next) => {
 
 /*****************************       게시판 정보 관리     ***************************** */
 
-router.post("/create_board", (req, res) => {
+router.post("/create_board",authUtil, (req, res) => {
   try {
     var description = req.body.description;
     var userid = req.body.userid;
@@ -107,19 +108,16 @@ router.post("/create_board", (req, res) => {
     res.json({ success: false });
   }
 });
-router.delete("/delete_board/:id", (req, res) => {
+router.delete("/delete_board/:id", authUtil, (req, res) => {
   try {
     var id = req.params.id;
     // var userid = req.body.userid;
-    db.query(
-      `delete from board where id=${id}`,
-      (err, results, field) => {
-        if (err) {
-          console.log(err);
-          res.json({ success: false });
-        } else res.json({ success: true });
-      }
-    );
+    db.query(`delete from board where id=${id}`, (err, results, field) => {
+      if (err) {
+        console.log(err);
+        res.json({ success: false });
+      } else res.json({ success: true });
+    });
   } catch (err) {
     console.log(err);
     res.json({ success: false });
@@ -127,7 +125,7 @@ router.delete("/delete_board/:id", (req, res) => {
 });
 
 // 조회수 증가
-router.get("/view_board/:id", (req, res) => {
+router.get("/view_board/:id", authUtil, (req, res) => {
   try {
     var boardid = req.params.id;
     // db 의 product 테이블에 상품을 등록하기 위한 코드
@@ -149,7 +147,7 @@ router.get("/view_board/:id", (req, res) => {
 });
 
 // 조회수 감소
-router.delete("/view_board/:id", (req, res) => {
+router.delete("/view_board/:id", authUtil, (req, res) => {
   try {
     var boardid = req.params.id;
     // db 의 product 테이블에 상품을 등록하기 위한 코드
@@ -171,7 +169,7 @@ router.delete("/view_board/:id", (req, res) => {
 });
 
 // 게시판 추천 추가
-router.get("/recommend_board/:id", (req, res) => {
+router.get("/recommend_board/:id", authUtil, (req, res) => {
   try {
     var boardid = req.params.id;
 
@@ -198,7 +196,7 @@ router.get("/recommend_board/:id", (req, res) => {
 });
 
 // 게시판 추천 취소
-router.delete("/recommend_board/:id", (req, res) => {
+router.delete("/recommend_board/:id", authUtil, (req, res) => {
   try {
     var boardid = req.params.id;
 
@@ -225,7 +223,7 @@ router.delete("/recommend_board/:id", (req, res) => {
 });
 
 /*************************************  유저 레벨업  **************************************** */
-router.post("/levelup", (req, res) => {
+router.post("/levelup", authUtil, (req, res) => {
   try {
     console.log("api create_product");
     var userid = req.body.userid;
@@ -265,43 +263,26 @@ router.post("/levelup", (req, res) => {
   }
 });
 
-router.post("/modify_count", (req, res) => {
-  try {
-    var barcode = req.body.barcode;
-    var type = req.body.type;
-    var userid = req.body.userid;
 
+
+router.get("/todolist", authUtil, (req, res) => {
+  try {
+    var userid = req.email;
+
+    // db 의 product 테이블에 상품을 삭제하기 위한 코드
     db.query(
-      `select count, product_id from barcode where userid='${userid}' and barcode_value='${barcode}'`,
+      `select userid, todoid, done from todolist where userid = ${userid}`,
       (err, results, field) => {
         if (err) {
           console.log(err);
           res.json({ success: false });
+          // 등록하는 과정에 에러가 있으면 {success: false} 반환
         } else {
-          let count = results[0].count;
-          let pid = results[0].product_id;
-          type = type === "1" ? "+" : "-";
-          db.query(
-            `update product set product_left = product_left ${type} ${count} where pid=${pid}`,
-            (err, results, field) => {
-              if (err) {
-                console.log(err);
-                res.json({ success: false });
-              } else {
-                db.query(
-                  `insert into log(userid, pid, type, count) values('${userid}', '${pid}', '${type}' , '${count}')`,
-                  (err, results, field) => {
-                    if (err) {
-                      console.log(err);
-                      res.json({ success: false });
-                    } else {
-                      res.json({ success: true });
-                    }
-                  }
-                );
-              }
-            }
-          );
+          res.json({
+            success: true,
+            list: results
+          });
+          // 성공적으로 삭제하였으면 {success : true} 반환
         }
       }
     );
@@ -310,17 +291,76 @@ router.post("/modify_count", (req, res) => {
     res.json({ success: false });
   }
 });
-
-router.get("/get_log/:userid", (req, res) => {
+router.get("/rank", authUtil, (req, res) => {
   try {
-    var userid = req.parmas.userid;
+    var userid = req.email;
+    // db 의 product 테이블에 상품을 삭제하기 위한 코드
     db.query(
-      `select * from log where userid = '${userid}'`,
+      `select email, name from user order by experience desc`,
       (err, results, field) => {
         if (err) {
           console.log(err);
           res.json({ success: false });
-        } else res.json({ success: true, list: results });
+          // 등록하는 과정에 에러가 있으면 {success: false} 반환
+        } else {
+          res.json({
+            success: true,
+            data: results,
+          });
+          // 성공적으로 삭제하였으면 {success : true} 반환
+        }
+      }
+    );
+  } catch (err) {
+    console.log(err);
+    res.json({ success: false });
+  }
+});
+router.post("/todolist", authUtil, (req, res) => {
+  try {
+    var userid = req.email;
+    var todoid = req.body.todoid;
+    // db 의 product 테이블에 상품을 삭제하기 위한 코드
+    db.query(
+      `select userid, todoid, done from todolist where userid = ${userid}`,
+      (err, results, field) => {
+        if (err) {
+          console.log(err);
+          res.json({ success: false });
+          // 등록하는 과정에 에러가 있으면 {success: false} 반환
+        } else {
+          res.json({
+            success: true,
+            list: results,
+          });
+          // 성공적으로 삭제하였으면 {success : true} 반환
+        }
+      }
+    );
+  } catch (err) {
+    console.log(err);
+    res.json({ success: false });
+  }
+});
+router.delete("/todolist", authUtil, (req, res) => {
+  try {
+    var userid = req.email;
+    var todoid = req.body.todoid;
+    // db 의 product 테이블에 상품을 삭제하기 위한 코드
+    db.query(
+      `delete from todolist where userid = ${userid}`,
+      (err, results, field) => {
+        if (err) {
+          console.log(err);
+          res.json({ success: false });
+          // 등록하는 과정에 에러가 있으면 {success: false} 반환
+        } else {
+          res.json({
+            success: true,
+            list: results,
+          });
+          // 성공적으로 삭제하였으면 {success : true} 반환
+        }
       }
     );
   } catch (err) {
